e43201d52f2f50fb5367e04b7df71277603cd7de
Leo Koo <leokoo@gmail.com>
2015-12-21 19:00:50 +0800

    added authentication with full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :fullname, presence: true, length: {maximum: 50}
 end

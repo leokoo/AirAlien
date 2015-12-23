7ba425b0fbff12da7b09c0c8c0f175a6225a78da
Leo Koo <leokoo@gmail.com>
2015-12-23 15:38:25 +0800

    fixed mini errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'pages#home'
   devise_for  :users, 
               :path => '', 
               :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
